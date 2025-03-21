b53ba42d310c7f694424ee7f6f4ea2eae6ffed26
Mahe Sami <mahendransamynathan@gmail.com>
2025-03-21 17:55:08 +0000

    Added MongoDB Package

--- obj/Debug/net9.0/ProductService.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3def3b917e70ee2c226a673316d6fb3ac6727feb")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
